92bdf9a8e24853d62d2c83480707b8e347badd58
Nicolas Mattia <nicolas@nmattia.com>
2025-01-01 21:42:18 +0100

    Drop remark42

--- astro.config.ts
+++ astro.config.ts
@@ -5,12 +5,11 @@ import rehypeRaw from "rehype-raw";
 import { rehypeShikiCommands } from "./plugins/rehype-shiki-commands";
 import { rehypeCopyCodeIntegration } from "./plugins/rehype-copy-code";
 import { remarkGfmAdmonitions } from "./plugins/remark-gfm-admonitions";
-import { remark42Integration } from "./plugins/remark42";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nmattia.com",
-  integrations: [sitemap(), rehypeCopyCodeIntegration(), remark42Integration()],
+  integrations: [sitemap(), rehypeCopyCodeIntegration()],
   markdown: {
     shikiConfig: {
       theme: "dark-plus",
