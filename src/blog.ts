bbef4315950ed62a045e934334f889ebb65f1620
Nicolas Mattia <nicolas@nmattia.com>
2026-09-20 22:47:35 +0200

    Modernize design (#209)
    * Modernize design
    
    This adds a date to each article on the front page, makes cards a bit
    more square and simplifies the responsive sizing.
    
    * Address review comments
    
    * Address review comments

--- src/blog.ts
+++ src/blog.ts
@@ -81,3 +81,19 @@ export const blogEntryPath = (blogPostEntry: CollectionEntry<"blog">) => {
 export const blogEntryTransitionName = (
   blogPostEntry: CollectionEntry<"blog">,
 ) => `blog-transition-${blogEntryName(blogPostEntry)}`;
+
+// Returns the date as "January 9, 2012"
+export const prettyDate = (d: Date) => {
+  return d.toLocaleString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: "UTC",
+  });
+};
+
+// Returns the date as "2012-01-09"
+export const dateToYYYYMMDD = (d: Date) => {
+  const fullString = d.toISOString(); /* 2011-10-05T14:48:00.000Z */
+  return fullString.split("T")[0];
+};
